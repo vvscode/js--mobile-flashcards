cd2aa2bea6c9a238254b5066dc8788bbc5c20c05
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-11-30 20:43:50 +0300

    Fix typos

--- utils/configureStore.js
+++ utils/configureStore.js
@@ -3,7 +3,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
-import reducer from "./reducers";
+import reducers from "../reducers";
 
 const middlewares = [thunk];
 
